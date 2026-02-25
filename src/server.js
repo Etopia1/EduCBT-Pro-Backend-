20a9df6fe5d63838568ead4d48db441211ca764f
Etopia1 <jolaetopia81@gmail.com>
2026-02-25 13:50:19 +0100

    new commit

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175"],
+        origin: [process.env.FRONTEND_URL, "http://localhost:5173", "http://localhost:5174", "http://localhost:5175"],
         methods: ["GET", "POST"]
     }
 });
@@ -19,7 +19,7 @@ app.set('io', io); // Allow access to io in controllers
 
 // Middleware
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175"],
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173", "http://localhost:5174", "http://localhost:5175"],
     credentials: true
 }));
 app.use(express.json());
